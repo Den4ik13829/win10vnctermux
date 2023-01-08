920f8c3dcc74146e767f8ff1a290221dcc30508b
Den4ik13829 <90059382+Den4ik13829@users.noreply.github.com>
2023-01-08 22:58:20 +0300

    Update win7.sh

--- win7.sh
+++ win7.sh
@@ -1,5 +1,5 @@
 apt-get update
 wget -O w7x64.img https://bit.ly/akuhnetw7X64
 apt-get install qemu-system-x86 -y
-qemu-system-x86_64 -hda w7x64.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+qemu-system-x86_64 -hda w7x64.img -m 4G -smp cores=4 -vnc:2 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 clear
